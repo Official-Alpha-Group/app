fa39b6f2359474a4660fd388fe7f22c082fc977e
lucif680 <pratikmazumdar680@protonmail.com>
2020-10-11 09:27:33 +0530

    Version 0.1.1

--- src/model/const.js
+++
@@ -1,5 +0,0 @@
-export const APP_NAME = 'ALPHA FOODS';
-export const APP_NAME_LOWER = 'alpha foods';
-export const REGISTER = 'Register/Login';
-export const SETTINGS = 'Settings';
-export const CARD_ONE_HEADER = 'Subscription Plan';
