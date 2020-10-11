f2b8ee27909c0230a9588d73e34fb24d4a20c205
lucif680 <pratikmazumdar680@protonmail.com>
2020-10-11 09:22:44 +0530

    Update

--- src/Activities/StartActivity.js
+++ src/Activities/StartActivity.js
@@ -1,64 +1,55 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity,Image } from 'react-native';
-import { Styles } from 'src/styles/styles';
-import * as Const from 'src/const';
-import { LinearGradient } from 'expo-linear-gradient';
-
-// This is used to create Cards for StartActivity only
-function Card(props) {
-  return (
-    <TouchableOpacity onPress={props.onClick}>
-      <LinearGradient
-        colors={['#1257d5', '#648fde']}
-        style={Styles.card}  >
-        <Text style={Styles.header}>
-          {props.header}
-        </Text>
-        <Text style={Styles.text}>
-          {props.text}
-        </Text>
-      </LinearGradient>
-    </TouchableOpacity>
-  );
-}
+import { Text, View, Image, ImageBackground } from 'react-native';
+import  {Styles,toPxlHeight,toPxlWidth}  from 'src/styles/styles';
+import * as Const from 'src/model/const';
+import { Button } from 'src/model/Button';
 
 export class StartActivity extends Component {
+  constructor(props) {
+    super(props);
+  }
   openSubscription = () => {
     this.props.navigation.navigate('SubscriptionActivity');
+
   }
-  openOneTime = () =>{
+  openOneTime = () => {
     this.props.navigation.navigate('OneTimeActivity');
   }
-  openRegisterNow = () =>{
+  openRegisterNow = () => {
     this.props.navigation.navigate('RegisterActivity');
   }
   render() {
     return (
-      <View style={Styles.startFragment}>
-        <Image
-          source={require('assets/img/logo.png')}
-          style={[Styles.logo, Styles.marginBot]} />
-        <Text style={Styles.appName}>
-          {Const.APP_NAME_LOWER}
+      <ImageBackground 
+      source={require('assets/img/vector.png')}
+      style={{width:toPxlWidth(100),height:toPxlHeight(100)}}>
+      <View style={Styles.center}>
+ 
+          <Image
+            source={require('assets/img/logo.png')}
+            style={[Styles.logo, Styles.marginBot]} />
+          <Text style={Styles.appName}>
+            {Const.APP_NAME_LOWER}
+          </Text>
+          <Button
+            header={Const.CARD_ONE_HEADER}
+            onClick={this.openSubscription}
+            animating={false}
+          />
+          <Button
+            header='One Time Purchase'
+            onClick={this.openOneTime}
+            animating={false} />
+          <Text style={[Styles.textBlack, Styles.marginTop, Styles.opactiy]}>
+            OR
         </Text>
-        <Card
-          header={Const.CARD_ONE_HEADER}
-          text='A monthly subscription plan for your day to day products.'
-          onClick = {this.openSubscription}
-        />
-        <Card
-          header='One Time Purchase'
-          text='Buy excellent products like dry fruits and day to day product on the go.' />
-        <Text style={Styles.textBlack, Styles.marginTop, Styles.opactiy}>
-          OR
-        </Text>
-        <Text 
-        style={[Styles.header, Styles.colorPrimary, Styles.marginTop]}
-        onPress={this.openRegisterNow}>
-          {Const.REGISTER}
-        </Text>
+          <Text
+            style={[Styles.textBold, Styles.colorPrimary, Styles.marginTop]}
+            onPress={this.openRegisterNow}>
+            {Const.REGISTER}
+          </Text>
       </View>
+      </ImageBackground>
     );
-
   }
 }
