13d8ba05ce1059b76bddaae09096bd6169070df7
Alpha Group <official@groupalpha.in>
2020-10-24 19:04:12 +0530

    Update index.js

--- actions/index.js
+++ actions/index.js
@@ -2,7 +2,7 @@ export const CONNECTED =  'CONNECTED';
 export const NOT_CONNECTED = 'NOT_CONNECTED';
 
 export function isConnected() {
-    fetch('http://192.168.29.237/Nacl-Dev-Program/main/test/')
+    fetch('http://example.com/ping')
         .then((response) => { return { type: CONNECTED }})
         .catch((error) => { return { type: NOT_CONNECTED }; })
 }
